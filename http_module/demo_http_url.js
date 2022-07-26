e9bd7514ee5198ddb993f14b6e70900ae53c8839
emediong <emediong5135@gmail.com>
2022-07-26 09:43:09 +0100

    initial commit

---
+++ http_module/demo_http_url.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.url);
+    res.end();
+}).listen(8080);
+
+// Try the address http://localhost:8080/summer and http://localhost:8080/winter
---
+++ npm/demo_uppercase.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var uc = require('upper-case');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    // Use our upper-case module to uppercase a string
+    res.write(uc.upperCase('Hello World!'));
+    res.end();
+}).listen(8080);
